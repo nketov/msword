8d697d63a8208581cf3a78190c26d6d3cc9457d7
Nikita Ketov <nketov@bigmir.net>
2017-04-22 20:11:53 +0300

    22_04_2017_2

--- js/form_script.js
+++ js/form_script.js
@@ -1,100 +1,79 @@
 $('document').ready(function () {
 
-    var uploadedFile = $("#file"), buttonClearUploaded = $("#buttonClearUploaded"), buttonUpload = $('#buttonUpload').find('span');
-    buttonClearUploaded.fadeOut();
-    //                                                                                          Hover Buttons
-    $('.button').hover(function() {
-        $(this).animate({ bottom: "7%", width: "145px"}, 100);
+    // var uploadedFile = $("#file"), buttonClearUploaded = $("#buttonClearUploaded"), buttonUpload = $('#buttonUpload').find('span');
 
-    },function(){        
-        $(this).animate({ bottom: "5%", width: "128px" }, 100);
-    });
-    
-//                                                                                             Uploader
-    var uploader = new AjaxUpload(buttonUpload, {
-        action: 'form_submit.php?button=upload',
 
-        onSubmit: function () {
-            buttonUpload.text('Загрузка... ');
-            this.disable();
-            $('#imageLoad').show();
-        },
 
-        onComplete: function (file) {
-            $('#imageLoad').hide();
-            uploadedFile.text(file);
-            uploadedFile.fadeIn(2000);
-            buttonClearUploaded.fadeIn(2000);
-            buttonUpload.text("Фаил загружен").css('opacity', '0.5');
-        }
+
+
+
+    //                                                                                              DataPickers
+    $('[name = "date"]').datepicker({
+        dateFormat: 'dd/mm/yy ', showAnim: 'show', defaultDate: '-1y', changeYear: true
     });
-    
-    buttonClearUploaded.on("click", function () {
-        uploader.enable();
-        $.get("form_submit.php?button=clearUploaded");
-        buttonUpload.text('Выбрать файл').css('opacity', '1');
-        uploadedFile.fadeOut(2000);
-        buttonClearUploaded.fadeOut(2000);
+
+    $('[name = "date1"]').datepicker({
+        dateFormat: 'dd/mm/yy ', showAnim: 'show', defaultDate: '-0d', changeYear: true
     });
-    
-    //                                                                                              DataPicker
-    $("#birthdate").datepicker({
-        dateFormat: 'dd/mm/yy ', showAnim: 'show', defaultDate: '-20y', changeYear: true
+
+    $('[name = "denial"]').datepicker({
+        dateFormat: 'dd/mm/yy ', showAnim: 'show', defaultDate: '+1m', changeYear: true
     });
+
 
     //                                                                                     Buttons Clicks
-    $("#right-button").on("click", function () {
-        $('#page1').fadeToggle(1000);
-        $('#page2').fadeToggle(2000);
-    });
+    // $("#right-button").on("click", function () {
+    //     $('#page1').fadeToggle(1000);
+    //     $('#page2').fadeToggle(2000);
+    // });
+    //
+    // $("#left-button").on("click", function () {
+    //
+    //     $('#page2').fadeToggle(1000);
+    //     $('#page1').fadeToggle(2000);
+    //
+    // });
 
-    $("#left-button").on("click", function () {
+    // $("#mail-button").on("click", function () {
+    //
+    //     $.post("form_submit.php?button=mail", {
+    //             firstName: $("#firstName").val(),
+    //             lastName: $("#lastName").val(),
+    //             sex: $('input:radio[name=sex]:checked').val(),
+    //             birthdate: $("#birthdate").val(),
+    //             optionallyInformation: $("#optionallyInformation").val(),
+    //             emailAddress: $("#emailAddress").val()
+    //         },
+    //
+    //         function (mailResult) {
+    //             $('#mailResult').append(mailResult);
+    //         });
+    //
+    //     $('#page2').fadeToggle(1000);
+    //     $('#page3').fadeToggle(2000);
+    // });
 
-        $('#page2').fadeToggle(1000);
-        $('#page1').fadeToggle(2000);
+    // $("#blank-button").on("click", function () {
+    //
+    //     $("#firstName").val("");
+    //     $("#lastName").val("");
+    //     $('input:radio[name=sex]').prop('checked', false);
+    //     $("#birthdate").val("");
+    //     $("#optionallyInformation").val("");
+    //     $("#emailAddress").val("");
+    //     buttonClearUploaded.click();
+    //     $('#mailResult').text('');
+    //
+    //     $('#page3').fadeToggle(1000);
+    //     $('#page1').fadeToggle(2000);
+    //
+    // });
 
-    });
-
-    $("#mail-button").on("click", function () {
-
-        $.post("form_submit.php?button=mail", {
-                firstName: $("#firstName").val(),
-                lastName: $("#lastName").val(),
-                sex: $('input:radio[name=sex]:checked').val(),
-                birthdate: $("#birthdate").val(),
-                optionallyInformation: $("#optionallyInformation").val(),
-                emailAddress: $("#emailAddress").val()
-            },
-
-            function (mailResult) {
-                $('#mailResult').append(mailResult);
-            });
-
-        $('#page2').fadeToggle(1000);
-        $('#page3').fadeToggle(2000);
-    });
-
-    $("#blank-button").on("click", function () {
-
-        $("#firstName").val("");
-        $("#lastName").val("");
-        $('input:radio[name=sex]').prop('checked', false);
-        $("#birthdate").val("");
-        $("#optionallyInformation").val("");
-        $("#emailAddress").val("");
-        buttonClearUploaded.click();
-        $('#mailResult').text('');
-
-        $('#page3').fadeToggle(1000);
-        $('#page1').fadeToggle(2000);
-
-    });
-
-    $("#close-button").on("click", function () {
-
-        location.assign("../../index.html");
-        buttonClearUploaded.click();
-    });
+    // $("#close-button").on("click", function () {
+    //
+    //     location.assign("../../index.html");
+    //     buttonClearUploaded.click();
+    // });
 
 });
 
--- msword.php
+++ msword.php
@@ -1,24 +1,74 @@
 <?php
 //(Отказ действует в срок до "___"__________  ____ г.)
 //Указываем путь до подготовленного документа
-$patternFile = 'pattern/pattern.docx';
-$name='Александр Валентинович';
-$docxFile = 'mails/'.$name.'.docx';
 
+
+$patternFile = 'pattern/pattern.docx';
+
+$docxFile = 'mails/'.$_POST['contractID'].'.docx';
+
+
+
+
+if (file_exists($docxFile)) unlink($docxFile);
 
 if (!copy($patternFile, $docxFile)) {
     echo "не удалось скопировать $file...\n";
 }
 
 
+
+
+
+$Month_r = array(
+    "01" => "января",
+    "02" => "февраля",
+    "03" => "марта",
+    "04" => "апреля",
+    "05" => "мая",
+    "06" => "июня",
+    "07" => "июля",
+    "08" => "августа",
+    "09" => "сентября",
+    "10" => "октября",
+    "11" => "ноября",
+    "12" => "декабря");
+
+
+
+
+$date=explode('/',$_POST['date']);
+$date1=explode('/',$_POST['date1']);
+$denial=explode('/',$_POST['denial']);
+
+
  
 //Список параметров
 $params = array(
-    '{{NAME1}}'    => $name."111"."Турчинов",
-    '{{NAME2}}'    => $name."222"."Турчинов",
-    '{{NAME3}}'    => $name."333"."Турчинов",
-    '{{NAME4}}'    => $name."444"."Турчинов",
-    '{{NAME5}}'    => $name."555"."Турчинов",
+
+    '{{CREDITOR}}'    => $_POST['creditor'] ? $_POST['creditor'] : " " ,
+    '{{CREDITOR_ADRESS}}'    => $_POST['creditorAdress']? $_POST['creditorAdress'] : " " ,
+
+    '{{DEBITOR}}'    => $_POST['debitor']? $_POST['debitor'] : " " ,
+    '{{DEBITOR_ADRESS}}'    => $_POST['debitorAdress']? $_POST['debitorAdress'] : " " ,
+    '{{PHONE}}'    => $_POST['phone']? $_POST['phone'] : " " ,
+    '{{EMAIL}}'    => $_POST['emailAddress']? $_POST['emailAddress'] : " " ,
+    '{{NUMBER}}'    => $_POST['contractID']? $_POST['contractID'] : " " ,
+
+    '{D}'    => $date[0] ,
+    '{MONTH}'    => "_".$Month_r[$date[1]]."_" ,
+    '{YEAR}'    => $date[2],
+
+    '{D1}'    => $date1[0] ,
+    '{MONTH1}'    => "_".$Month_r[$date1[1]]."_" ,
+    '{YEAR1}'    => $date1[2],
+
+    '{{DENIAL}}'    => ($_POST['ch']==="on") ? '(Отказ действует в срок до '.$denial[0] .' '.$Month_r[$denial[1]].' '. $denial[2].'г.)' : " " ,
+
+
+
+
+
 
 );
  
